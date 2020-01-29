a57b1eeeb96689b6f2e556d38baf951d7e01fe4f
Paul James <p.james@coolblue.nl>
2020-01-29 09:42:38 +0100

    Setup token

--- index.js
+++ index.js
@@ -3,16 +3,17 @@ import * as github from '@actions/github';
 
 async function run() {
     // This should be a token with access to your repository scoped in as a secret.
-    // The YML workflow will need to set myToken with the GitHub Secret Token
-    // myToken: ${{ secrets.GITHUB_TOKEN }}
+    // The YML workflow will need to set githubToken with the GitHub Secret Token
+    // githubToken: ${{ secrets.GITHUB_TOKEN }}
     // https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret
-    const myToken = core.getInput('myToken');
+    const githubToken = core.getInput('repo-token');
 
-    const octokit = new github.GitHub(myToken);
+    const octokit = new github.GitHub(githubToken);
 
     if (github.context.eventName === 'push') {
         const pushPayload = github.context.payload;
         core.info(`The head commit is: ${pushPayload.head}`);
+        core.setOutput('head', pushPayload.head);
     }
 }
 
